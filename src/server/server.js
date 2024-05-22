73aa4609a321cde42277288da83045ca5a248133
IsmailZhanfeari <ismailzhanfeari@gmail.com>
2024-05-22 12:32:14 +0700

    -

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0',
+        port: 8080,
+        host: "0.0.0.0",
         routes: {
             cors: {
                 origin: ["*"],
