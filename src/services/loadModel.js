a480437084dc25e53616cdd9aff5d52674125ac2
IsmailZhanfeari <ismailzhanfeari@gmail.com>
2024-05-22 12:07:32 +0700

    -

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const tf = require("@tensorflow/tfjs-node");
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    return tf.loadGraphModel("https://storage.googleapis.com/model-storage-ismail/submissions-model/model.json");
 }
 module.exports = loadModel;
